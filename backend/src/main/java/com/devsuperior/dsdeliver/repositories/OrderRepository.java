7aef9dd8e97852be2a8ad717434fa69ea939ad6a
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-08 21:11:15 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
@@ -1,6 +1,9 @@
 package com.devsuperior.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dsdeliver.entities.Order;
@@ -8,4 +11,6 @@ import com.devsuperior.dsdeliver.entities.Order;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+	@Query("SELECT DISTINCT o FROM Order o JOIN FETCH o.products WHERE o.status = 0 ORDER BY o.moment ASC")
+	List<Order> findOrdersWithProducts();
 }
