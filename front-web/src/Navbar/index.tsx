64aa401129606cea7c5d65d95000485be4846b8f
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-10 17:46:33 -0300

    Adding Routes and Steps

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -1,10 +1,13 @@
 import './styles.css';
 import { ReactComponent as Logo } from './logo.svg';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <nav className="main-navbar">
-            <Logo />
+            <Link to="/">
+                <Logo />
+            </Link>
         </nav>
     )
 }
